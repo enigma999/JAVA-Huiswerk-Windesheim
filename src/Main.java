ca065b69c6f46f26f285013a05f322b2add8c003
stanb <Stanborst@gmail.com>
2023-02-10 17:49:00 +0100

    huiswerk week 1

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
 
 public class Main {
     public static void main(String[] args) {
-        Hemellichaam hl5 = new Hemellichaam("Antares", "ster");
+        Taxirit rit1 = new Taxirit(400.35);
+        Taxirit rit2 = new Taxirit(400.35,4,7);
 
-        hl5.setTemperatuur(3204);
-
-        hl5.printTemperatuur();
+        rit2.voegPersoonToe();
+        rit1.voegPersoonToe();
+        rit1.voegPersoonToe(20);
+        rit2.voegPersoonToe(3);
+        System.out.println(rit1.berekenPrijsPerPersoon());
+        System.out.println(rit1.berekenPrijsPerPersoon(30));
     }
 }
--- src/Taxirit.java
+++ src/Taxirit.java
@@ -1,2 +1,49 @@
 public class Taxirit {
+    private double prijsPerKm = 1.25;
+    private int aantalPers;
+    private int maxAantal;
+    private double afstand;
+
+    public Taxirit(double afstand){
+        this.afstand=afstand;
+    }
+
+    public Taxirit(double afstand, int aantalPers, int maxAantal){
+        this.afstand= afstand;
+        this.aantalPers= aantalPers;
+        this.maxAantal= maxAantal;
+    }
+
+    public void voegPersoonToe(){
+            if(this.aantalPers<this.maxAantal){
+                this.aantalPers++;
+            }else{
+                System.out.println("Sorry de taxi zit vol.");
+            }
+    }
+
+    public void voegPersoonToe(int aantalPers){
+        if((this.aantalPers+aantalPers)<=this.maxAantal){
+            this.aantalPers=+aantalPers;
+        }else{
+            System.out.println("Sorry dit past niet.");
+        }
+    }
+
+    public double berekenPrijsPerPersoon(){
+        double ritPrijs=this.afstand*this.prijsPerKm;
+        if(this.aantalPers==this.maxAantal){
+            ritPrijs=ritPrijs*0.9;
+        }
+        return ritPrijs;
+    }
+
+    public double berekenPrijsPerPersoon(double koritingsPercentage){
+        double ritPrijs=this.afstand*this.prijsPerKm;
+        double kortingsfactor=1-(koritingsPercentage/100);
+        ritPrijs=ritPrijs*kortingsfactor;
+
+        return ritPrijs;
+    }
+
 }
