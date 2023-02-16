18ccd0258155ae1290aed0a518566b41df83d90d
enigma999 <Stanborst@gmail.com>
2023-02-16 20:58:15 +0100

    remove idea map

--- src/Laptop.java
+++ src/Laptop.java
@@ -14,7 +14,41 @@ public class Laptop {
 
     public Laptop(String naam, String OS, int schermdiagonaal) {
         this.naam = naam;
+        if(OS)
         this.OS = OS;
         this.schermdiagonaal = schermdiagonaal;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public String getOS() {
+        return OS;
+    }
+
+    public int getSchermdiagonaal() {
+        return schermdiagonaal;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public void setOS(String OS) {
+        this.OS = OS;
+    }
+
+    public void setSchermdiagonaal(int schermdiagonaal) {
+        this.schermdiagonaal = schermdiagonaal;
+    }
+
+    @Override
+    public String toString() {
+        return "Laptop{" +
+                "naam='" + naam + '\'' +
+                ", OS='" + OS + '\'' +
+                ", schermdiagonaal=" + schermdiagonaal +
+                '}';
     }
 }
