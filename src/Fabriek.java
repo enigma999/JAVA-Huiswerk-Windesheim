301f93199ca7c9b78df222cfc5da280d83cc2ccd
enigma999 <stanborst@gmail.com>
2023-02-17 21:52:44 +0100

    bijeenkomst 2.3 afgemaakt tot opdacht 3b

--- src/Fabriek.java
+++ src/Fabriek.java
@@ -1,6 +1,17 @@
 public class Fabriek {
     private String naam;
+    int aantalGemaakteAutos;
+    static int totaalAantalGemaakteAutos;
     public Fabriek() {
         this.naam="tesla";
+        this.aantalGemaakteAutos=0;
     }
+
+    public Auto maakAuto(){
+        String type = "Model 3";
+        this.aantalGemaakteAutos++;
+        totaalAantalGemaakteAutos++;
+        return new Auto(this.naam,type,360);
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("aantal: " +
+                Fabriek.totaalAantalGemaakteAutos); // 0
+        Fabriek fabriek = new Fabriek();
+        Auto a = fabriek.maakAuto();
+        Auto bb = fabriek.maakAuto();
+        System.out.println("aantal: " +
+                Fabriek.totaalAantalGemaakteAutos); // 2
+        Fabriek fabriekTwee = new Fabriek();
+        Auto ccc = fabriekTwee.maakAuto();
+        Auto dddd = fabriekTwee.maakAuto();
+        Auto eeeee = fabriek.maakAuto();
+        System.out.println("aantal: " +
+                Fabriek.totaalAantalGemaakteAutos); // 5
+        System.out.println(fabriekTwee.aantalGemaakteAutos);
+        System.out.println(fabriek.aantalGemaakteAutos);
 
     }
 }
