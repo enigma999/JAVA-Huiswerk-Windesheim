f8dd9c8e20c66247e916cb1bd596e60ef3b35d04
enigma999 <stanborst@gmail.com>
2023-02-17 17:58:00 +0100

    main leeggemaakt

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,6 @@
 
 public class Main {
     public static void main(String[] args) {
-        Student s1 = new Student();
-        Student s2 = new Student();
-        s1.naam = "Piet";
-        s1.nummer = 5236;
-        s2.naam = "Angelo";
-        s2.nummer = 8569;
-        KbsGroep groep1 = new KbsGroep();
-        s1.setGroep(groep1);
-        s2.setGroep(groep1);
-        Laptop laptop1=new Laptop("pc","windows",24);
+
     }
 }
